c4150097fc4ecce77b94d37dba18c0c9a1f7fd6c
Patiphan Phengpao <misterpatipan123@gmail.com>
2024-08-29 23:56:56 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
+import HomePage from './home/HomePage'
+import Logo3 from './assets/logo-3.svg'
+
+import ProjectsPage from './projects/ProjectsPage'
+import ProjectPage from './projects/ProjectPage'
+
+function App() {
+  return (
+    <Router>
+      <header className='sticky'>
+        <span className='logo'>
+          <img src={Logo3} alt="logo" width="49" height="99" />
+        </span> 
+        <NavLink to="/" className="button rounded">
+          <span className='icon-home'></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ todo.md
@@ -0,0 +1,22 @@
+[x] Lab 1: Creating a New Project
+[x] Lab 2: Running Your Project
+[x] Lab 3: Styles Using CSS
+[x] Lab 4: Your First Component
+[x] Lab 5: Creating Data Structures
+[x] Lab 6: Passing Data into a Component
+[x] Lab 7: Displaying List Data
+[x] Lab 8: More Reusable Components
+[x] Lab 9: Responding to an Event
+[x] Lab 10: Create a Form
+[x] Lab 11: Communicating from Child to Parent Component
+[x] Lab 12: Hiding and Showing Components
+[x] Lab 13: More Component Communication
+[x] Lab 14: Multiple Levels of Component Communication
+[x] Lab 15: Form Values to State
+[x] Lab 16: Form Validation
+[] Lab 17: Setup Backend REST API
+[] Lab 18: HTTP GET
+[] Lab 19: HTTP PUT
+[] Lab 20: Router Basics
+[] Lab 21: Route Parameters
+[] Lab 22: Build & Deploy
